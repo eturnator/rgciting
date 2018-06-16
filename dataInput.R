4fa34ed63805abb835cc1db8ac2304dd0a89de27
sunnyonyxtom <sunnyonyxtom@gmail.com>
2018-06-16 15:03:28 -0600

    Added Function to generate AID

--- dataInput.R
+++ dataInput.R
@@ -1,26 +1,20 @@
-
 dataSetA <- read.delim("data/WOS_SciConf_ResearchGate_20180531.txt", sep="\t", header = TRUE, row.names = NULL, fill = TRUE)
 dataSetB <- read.delim("data/WOS_SciConf_Research_Gate_20180531.txt", sep="\t", header = TRUE, row.names = NULL, fill = TRUE)
 dataSetC <- read.delim("data/WOS_SciSocSci_ResearchGate_20180531.txt", sep="\t", header = TRUE, row.names = NULL, fill = TRUE)
 dataSetD <- read.delim("data/WOS_SciSocSci_Research_Gate_20180531.txt", sep="\t", header = TRUE, row.names = NULL, fill = TRUE)
 
-dataSets <- list(dataSetA, dataSetB, dataSetC, dataSetD)
-rownames(dataSetA) <- NULL
+#function to remove empty columns
 cleanColumn <- function(data){
   emptyColumns <- sapply(data, function (x) all(is.na(x)))
   data <- data[!emptyColumns]
 }
 
-
+#function to generate and append AID
 createAID <- function(data, id) {
-  numElements <- nrow(dataSetA)
-  names <- str(0:(numElements - 1))
- # names <- lapply(names, function(x) x)
+  AID <- seq(nrow(data))
+  AID <- paste(id, formatC(AID, width=3, flag="0"), sep="")
+  data <- cbind(data, AID)
 }
 
-createAID(dataSetA, 'C')
-
-#for (i in range(0, length(dataSets))){
-#  dataSets[i] <- cleanColumn(dataSets[i])
-#}
-print(dataSetA)
+dataSetA <- cleanColumn(dataSetA)
+dataSetA <- createAID(dataSetA, 'A')
