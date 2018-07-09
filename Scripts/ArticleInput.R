10ed6737d3a5846882430a22e8382a85ec8e558c
sunnyonyxtom <sunnyonyxtom@gmail.com>
2018-07-09 13:49:54 -0600

    Updated cleancolumn function and added citation info

--- Scripts/ArticleInput.R
+++ Scripts/ArticleInput.R
@@ -36,16 +36,34 @@ unique_SciSocSci <- setdiff(WOS_SciSocSci, WOS_SciConf)
 #remove columns with all NA values from WOS_SciConf as the main dataset for further processing
 WOS_SciConf <- WOS_SciConf[ , ! apply(WOS_SciConf, 2, function(x) all(is.na(x)))]
 
-#function to remove empty columns and generate AID
+#function to duplicate articles with more than 1 cited work generate AID
 CleanColumn <- function(data, id){
+  #generate AID
   AID <- paste(id, formatC(seq(nrow(data)), width=3, flag="0"), sep="")
-  new_col_names <- c("CitedAs", "Locatable", "CitationDetail_RG", "DOI_RG", "AuthorUpload",
-                    "SourcePubs", "SourcePreprint", "SourceRepo", "SourceWebsite", "SourceURl",
-                    "SourceDOI","ItemClass", "Copyrighted", "Licensed", "HCISI")
+  data <- cbind(AID, data)
+  
+  #duplicate articles with more than 1 times cited
+  for(i in 1:nrow(data)) {
+    for(j in 0:data[i,"TC"]){
+      if(j > 1){
+        data <- rbind(data, data[i,])
+      }
+    }
+  }
+  
+  #sort table
+  data <- data[order(data$"AID"),]
+ 
+  #append new columns
+  new_col_names <- c("CID", "CitedAs", "Locatable", "CitationDetail_RG", "FulltextAvailable", "DOI_RG", "AuthorUpload",
+                     "SourcePubs", "SourcePreprint", "SourceRepo", "SourceWebsite", "SourceURl",
+                     "SourceDOI","ItemClass", "Copyrighted", "Licensed", "HCISI")
   new_cols <- matrix(nrow=nrow(data), ncol=length(new_col_names))
   colnames(new_cols) <- new_col_names 
-  data <- cbind(AID, new_cols, data)
+  AID <- data$"AID"
+  data <- cbind(AID, new_cols, data[,-1])
 }
+
 
 #run cleanColumn function over data sets
 WOS_SciConf <- CleanColumn(WOS_SciConf, 'P')
